e5862893601e9a445eadbc928cb243055a32fdd5
Santiago Dedich <60894067+Santi3226@users.noreply.github.com>
2025-07-30 13:37:15 -0300

    Clase Login y token

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,3 +20,30 @@ function App() {
 }
 
 export default App
+
+/*
+JWT Json web token
+JWT crea un token que se decripta con una contraseña en el bend tipo base64
+JWT-decode libreria para manejar los token
+
+En el authprovider:
+
+const response = await axiosInterceptor creo/fetch/axios.post('routa del login', userData) ESTO VINCULA FRONT Y BACK
+localStorage.setItem('token', response.data.token) ESTO GUARDA LA SESION EN LA CACHE DEL BROWSER
+seIsAuthenticated(true)
+setUser(getUser(response.data.token)) GETUSER ES UNA FUNCION CON EL DECRIPTADOR QUE PASA EN LIMPIO LOS DATOS
+aca se verifica la expiracion tambien con la diferencia de Date.now()/1000 > tokenDecodeado.exp
+
+
+Integrar login, debe volver solo un token
+Decodear JWT con DecodeJWT por ej
+Persistir JWT en localStorage
+Agregar roles a las routes protected
+Ocultar items a los no autorizados
+Verificar expiracion del token jwt
+Interceptor axios, chequear en backnd
+
+Agregar authProvider
+Fetch o Axios para peticiones 
+
+*/
