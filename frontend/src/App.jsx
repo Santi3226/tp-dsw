652d1f572c86ef28c354728cfbaec7679f63b9d4
Santi3226 <60894067+Santi3226@users.noreply.github.com>
2025-10-03 22:00:32 -0300

    Redirects

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -47,13 +47,13 @@ function App() {
                 
                 <Route path="/paciente/:tab" element={
                   <ProtectedRoute>
-                      <Paciente allowedRoles={["user","admin"]}/>
+                      <Paciente allowedRoles={["Paciente","admin"]}/>
                   </ProtectedRoute>
                 }></Route>
 
                 <Route path="/turno/:tab" element={
                   <ProtectedRoute>
-                      <Turno allowedRoles={["user","admin"]}/>
+                      <Turno allowedRoles={["Paciente","admin"]}/>
                   </ProtectedRoute>
                 }></Route>
 
